f307a50baa245ae0951e054955300a1a22e3d587
OE-KT <brookskentaro@gmail.com>
2023-11-26 16:23:28 -0600

    - Added Loot tables for aged settlement

--- src/main/java/com/oekt/oerocks/util/ModTags.java
+++ src/main/java/com/oekt/oerocks/util/ModTags.java
@@ -13,9 +13,9 @@ public class ModTags {
     public static class Blocks {
         public static final TagKey<Block> HAMMER_MINEABLE = tag("mineable/hammer");
 
-        public static final TagKey<Block> NEEDS_STONE = tag("needs_stone");
-
-        public static final TagKey<Block> NEEDS_IRON = tag("needs_iron");
+//        public static final TagKey<Block> NEEDS_STONE = tag("needs_stone");
+//
+//        public static final TagKey<Block> NEEDS_IRON = tag("needs_iron");
         private static TagKey <Block> tag(String name) {
             return BlockTags.create(new ResourceLocation(OErocks.MODID, name));
         }
