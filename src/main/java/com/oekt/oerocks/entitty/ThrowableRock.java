ac28027cb9d59c488b38178dba5e3eeaf1081969
OE-KT <brookskentaro@gmail.com>
2023-12-06 21:15:32 -0600

    Added Basic System for rock abiltys

--- src/main/java/com/oekt/oerocks/entitty/ThrowableRock.java
+++ src/main/java/com/oekt/oerocks/entitty/ThrowableRock.java
@@ -13,6 +13,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
+import net.minecraftforge.common.Tags;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -78,6 +79,67 @@ public class ThrowableRock extends ThrowableItemProjectile {
         OErocks.LOGGER.info(rockCc.getRockType().toString());
         double Damage = rockCc.getDamege()*(this.getItem().getOrCreateTag().getDouble("power")+1);
         entity.hurt(this.damageSources().thrown(this, this.getOwner()),(float) Damage); // Detirms behavior onHit
+        EntittytypeHit(result, rockCc);
+//        if(rockCc.getRockType()!=null) {
+//            switch(rockCc.getRockType()) {
+//                case FIRE:
+//                    entity.setSecondsOnFire(10);
+//                    break;
+//                case ICE:
+//                    entity.setTicksFrozen(300);
+//                    break;
+//                default:
+//                    break;
+//            }
+//        }
+
+
+        super.onHitEntity(result);
+    }
+
+
+    @Override
+    protected void onHit(HitResult result) {
+        if(!(this.getItem().getItem() instanceof Rock rockCc)) {
+            return;
+        }
+        super.onHit(result);
+        if (!this.level().isClientSide) {
+            this.level().broadcastEntityEvent(this, (byte) 3);
+            typeHit(result, rockCc);
+
+
+        }
+
+
+    }
+
+    @Override
+    public void onAddedToWorld() {
+
+        super.onAddedToWorld();
+    }
+
+    public void typeHit(HitResult result, Rock rockCc) {
+        if(rockCc.getRockType()!=null) {
+            switch(rockCc.getRockType()) {
+                case FIRE:
+                    this.discard();
+                    break;
+                case ICE:
+                    //this.setDeltaMovement(this.getDeltaMovement().multiply(-0.7, -0.7, -0.7));
+                    this.discard();
+                    break;
+                default:
+                    this.discard();
+                    break;
+            }
+        }
+
+    }
+    public void EntittytypeHit(EntityHitResult result, Rock rockCc) {
+
+        Entity entity = result.getEntity();
         if(rockCc.getRockType()!=null) {
             switch(rockCc.getRockType()) {
                 case FIRE:
@@ -91,25 +153,8 @@ public class ThrowableRock extends ThrowableItemProjectile {
             }
         }
 
-
-        super.onHitEntity(result);
     }
 
-
-    @Override
-    protected void onHit(HitResult result) {
-
-        super.onHit(result);
-        if (!this.level().isClientSide) {
-            this.level().broadcastEntityEvent(this, (byte) 3);
-            this.discard();
-
-
-
-        }
-
-
-    }
 
 
 }
